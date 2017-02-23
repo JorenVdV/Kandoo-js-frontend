1d3f7a6e0675c6443b38527b64bce53d1c821821
Nick Jorens <nickjorens@gmail.com>
2017-02-23 14:48:25 +0100

    Pretty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,27 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule, BaseRequestOptions} from '@angular/http';
 
-import {AppComponent} from './app.component';
-import {ThemeComponent} from "./theme/details/theme.component";
+import {AuthGuard} from "./guards/auth.guard";
 import {AppRouting} from "./routing/app.routing";
-import {ThemeService} from "./services/theme.service";
-import {AlertComponent} from "./directives/alert.component";
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
-import {CardComponent} from "./card/card.component";
 import {MockBackend} from "@angular/http/testing";
 import {CardService} from "./services/card.service";
 import {UserService} from "./services/user.service";
-import {AuthenticationService} from "./services/authentication.service";
+import {RouterModule} from "@angular/router";
 import {AlertService} from "./services/alert.service";
-import {AuthGuard} from "./guards/auth.guard";
-import {CardDetailComponent} from "./card/card-detail.component";
+import {AppComponent} from './app.component';
+import {ThemeService} from "./services/theme.service";
+import {HomeComponent} from "./home/home.component";
+import {CardComponent} from "./card/card.component";
+import {GameComponent} from "./game/game.component";
+import {AlertComponent} from "./directives/alert.component";
+import {ThemeComponent} from "./theme/details/theme.component";
+import {LoginComponent} from "./login/login.component";
 import {SessionService} from "./services/session.service";
 import {SessionComponent} from "./session/session.component";
-import {GameComponent} from "./game/game.component";
+import {RegisterComponent} from "./register/register.component";
 import {ThemeListComponent} from './theme/theme-list/theme-list.component';
-import {RouterModule} from "@angular/router";
+import {CardDetailComponent} from "./card/card-detail.component";
+import {AuthenticationService} from "./services/authentication.service";
 
 
 @NgModule({
