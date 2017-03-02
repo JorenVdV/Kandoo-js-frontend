714213a13e35a6b0e5ccfaef8975b25d2cae30ef
Koen <koen.vercauteren@student.kdg.be>
2017-03-02 15:09:55 +0100

    cards + themelist tests

--- src/app/theme/list/theme-list.component.ts
+++ src/app/theme/list/theme-list.component.ts
@@ -36,7 +36,7 @@ export class ThemeListComponent implements OnInit {
     deleteTheme(theme: Theme) {
         this.themeService.deleteTheme(theme._id).subscribe(
             done => {
-                //this.themes.splice(t => t._id === theme._id, 1);
+                this.themes.splice(t => t._id === theme._id, 1);
             },
             err => {
                 console.log(err);
